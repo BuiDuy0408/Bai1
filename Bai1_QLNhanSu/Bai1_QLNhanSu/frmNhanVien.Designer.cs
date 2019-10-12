3cae44621281266124da125ca633aba6b97af5fb
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-10-12 22:05:51 +0700

    Thanh-addfrmNhanVien

---
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Bai1_QLNhanSu
+{
+    partial class frmNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Yellow;
+            this.label11.Location = new System.Drawing.Point(165, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(246, 24);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "THÔNG TIN NHÂN VIÊN";
+            // 
+            // frmNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 269);
+            this.Controls.Add(this.label11);
+            this.Name = "frmNhanVien";
+            this.Text = "frmNhanVien";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+    }
+}
