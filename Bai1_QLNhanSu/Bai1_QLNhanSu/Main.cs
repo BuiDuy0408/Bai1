5863dee4eb2ebc2c78ae64035d4e55d92eb8bcb0
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-10-13 13:25:14 +0700

    Thanh-decorateTicker

--- Bai1_QLNhanSu/Bai1_QLNhanSu/Main.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/Main.cs
@@ -19,17 +19,17 @@ namespace Bai1_QLNhanSu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            label1.Text = label1.Text.Substring(2, label1.Text.Length - 2) + label1.Text.Substring(0, 2);
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-
+            label1.ForeColor = Color.Yellow;
         }
 
         private void timer3_Tick(object sender, EventArgs e)
         {
-
+            label1.ForeColor = Color.Red;
         }
     }
 }
