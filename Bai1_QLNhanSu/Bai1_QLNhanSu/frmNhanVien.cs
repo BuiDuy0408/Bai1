d50f8f08b88c856d11e734a8ec49dcfe64b78dc3
truong19961405 <54049414+truong19961405@users.noreply.github.com>
2019-10-13 20:11:26 +0700

    xuantruong xoa

--- Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.cs
@@ -75,6 +75,19 @@ namespace Bai1_QLNhanSu
             MoDieuKhien();
             chon = 2;
         }
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (txtMaNV.Text == "")
+                MessageBox.Show("Chọn nhân viên!");
+            else
+            if (DialogResult.Yes == MessageBox.Show("Bạn muốn xóa nhân viên này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                nhanvien.XoaNhanVien(txtMaNV.Text);
+                MessageBox.Show("Xóa thành công!");
+                frmNhanVien_Load(sender, e);
+                SetNull();
+            }
+        }
 
     }
 }
