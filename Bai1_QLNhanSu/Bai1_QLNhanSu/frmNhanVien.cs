822a6d0ced264368d09128f8d80d90036a8b7652
truongmta190298 <camamidol@gmail.com>
2019-10-13 20:06:14 +0700

    ductruong setnull

--- Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using BangQLCT;
 namespace Bai1_QLNhanSu
 {
     public partial class frmNhanVien : Form
@@ -16,6 +16,9 @@ namespace Bai1_QLNhanSu
         {
             InitializeComponent();
         }
+        BUS_NhanVien nhanvien = new BUS_NhanVien();
+        int chon = 0;
+        TimKiem tk = new TimKiem();
         void KhoaDieuKhien()
         {
             txtMaNV.Enabled = txtHoDem.Enabled = txtTenNV.Enabled = txtGT.Enabled = dtpNgaySinh.Enabled = txtDiaChi.Enabled = txtSDT.Enabled = txtLuong.Enabled = txtChucVu.Enabled = cbMa_NQL.Enabled = cbMaDV.Enabled = false;
@@ -27,6 +30,23 @@ namespace Bai1_QLNhanSu
             txtMaNV.Enabled = txtHoDem.Enabled = txtTenNV.Enabled = txtGT.Enabled = dtpNgaySinh.Enabled = txtDiaChi.Enabled = txtSDT.Enabled = txtLuong.Enabled = txtChucVu.Enabled = cbMa_NQL.Enabled = cbMaDV.Enabled = true;
             btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = false;
             btnLuu.Enabled = true;
+        }
+        void SetNull()
+        {
+            txtMaNV.Text = txtHoDem.Text = txtTenNV.Text = txtGT.Text = txtSDT.Text = txtDiaChi.Text = txtLuong.Text = "";
+            txtChucVu.Text = cbMa_NQL.Text = cbMaDV.Text = "";
+            dtpNgaySinh.Text = DateTime.Now.ToShortDateString();
+            tscbGT.Text = tstxtDiaChi.Text = tstxtMa.Text = tstxtTen.Text = "";
+        }
+
+
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = nhanvien.HienThiNhanVien();
+            dgvNhanVien.DataSource = dt;
+            KhoaDieuKhien();
+            
         }
 
     }
