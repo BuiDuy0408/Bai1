cabdc2a02396298c4fe3700c7b1da5c936748914
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-10-13 15:36:35 +0700

    Thanh-add button Dang Nhap

--- Bai1_QLNhanSu/Bai1_QLNhanSu/frmDangNhap.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmDangNhap.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BangQLCT;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,20 @@ namespace Bai1_QLNhanSu
         public frmDangNhap()
         {
             InitializeComponent();
+        }
+        BUS_DangNhap dn = new BUS_DangNhap();
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if (dn.DangNhap(txtUserName.Text, txtPass.Text) == true)
+            {
+                // bool x = false;
+                MessageBox.Show("Bạn đăng nhập thành công ^^", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Main frm1 = new Main();
+                frm1.Show();
+                Hide();
+            }
+            else MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai. Mời bạn nhập lại !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
