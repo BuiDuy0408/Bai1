4f50df0cfa1ce6f623223466a40721158e5d81ed
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-10-13 15:32:55 +0700

    Thanh-add Connect File

--- Bai1_QLNhanSu/Bai1_QLNhanSu/frmHuongDan.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmHuongDan.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,43 @@ namespace Bai1_QLNhanSu
         public frmHuongDan()
         {
             InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+
+        private void trViewGioiThieu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Name == "gtPhanMem")
+            {
+                GetFileAll("GioiThieuChung.txt");
+                Image img = Image.FromFile(@"Nhansu.jpg");
+                pictureBox1.BackgroundImage = img;
+            }
+            else
+                if (e.Node.Name == "gtDangNhap")
+            {
+                GetFileAll("PhanDangNhap.txt");
+                Image img = Image.FromFile(@"anh dang nhap.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else
+                    if (e.Node.Name == "gtManHinhChinh")
+            {
+                GetFileAll("PhanMain.txt");
+                Image img = Image.FromFile(@"anh main.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else
+                        if (e.Node.Name == "gtQLNS")
+            {
+                GetFileAll("PhanQuanLi.txt");
+                Image img = Image.FromFile(@"anh nhan vien.png");
+                pictureBox1.BackgroundImage = img;
+            }
         }
     }
 }
