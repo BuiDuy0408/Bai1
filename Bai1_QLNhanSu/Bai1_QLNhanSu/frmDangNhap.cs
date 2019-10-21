599c8061a9550b168bf901aa355a2218ca9d444a
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-10-21 21:08:02 +0700

    Hoc-delete wrong part

--- Bai1_QLNhanSu/Bai1_QLNhanSu/frmDangNhap.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmDangNhap.cs
@@ -44,19 +44,6 @@ namespace Bai1_QLNhanSu
             timer2.Start();
             timer3.Start();
         }
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            label1.Text = label1.Text.Substring(2, label1.Text.Length - 2) + label1.Text.Substring(0, 2);
-        }
 
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            label1.ForeColor = Color.Yellow;
-        }
-
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            label1.ForeColor = Color.Red;
-        }
     }
 }
