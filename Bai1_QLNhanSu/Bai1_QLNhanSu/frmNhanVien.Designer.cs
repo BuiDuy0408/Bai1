b68dd912b0393ce457a6b972b9b086b0e60bb84d
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-10-21 21:11:09 +0700

    Hoc-Modified button Xoa

--- Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.Designer.cs
+++ Bai1_QLNhanSu/Bai1_QLNhanSu/frmNhanVien.Designer.cs
@@ -262,7 +262,7 @@
             this.btnXoa.TabIndex = 24;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
-            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            //this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnSua
             // 
